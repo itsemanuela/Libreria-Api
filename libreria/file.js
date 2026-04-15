5d14eb26d03ebfa4bb524568b13ce3b011dd355a
itsemanuela <notemanu152@gmail.com>
2026-04-15 14:58:40 +0200

    Commit

---
+++ libreria/file.js
@@ -0,0 +1,46 @@
+// recuperiamo le informazioni dall'api library per effettuare un fetch delle informazioni che mi servono
+const apidata = "https://striveschool-api.herokuapp.com/books  ";
+
+const book = function () {
+  fetch(apidata)
+    .then((response) => {
+      console.log(response);
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("La risposta che cerchi non esiste");
+      }
+    })
+    .then((arraydilibri) => {
+      const contenitore = document.getElementById("contenitore-card");
+      arraydilibri.forEach((libri) => {
+        const codice = libri.asin;
+        const titolo = libri.title;
+        const copertina = libri.img;
+        const prezzo = libri.price;
+        const genere = libri.category;
+        console.log(arraydilibri);
+
+        contenitore.innerHTML += ` 
+       <div class= "col-6 col-md-3 col-lg-2 mb-4">
+        <div class="card" >
+  <img src=${copertina} class="card-img-top" alt=${copertina}>
+  <div class="card-body">
+    <h5 class="card-title">${titolo}</h5>
+    <p class="card-text">${genere}</p>
+    <p class="card-text"> ${prezzo}</p>
+    <p class="card-text"> ${codice}</p>
+    <a href="#" class="btn btn-primary">Acquista</a>
+  </div>
+</div>   `;
+      });
+    })
+
+    //manipolo il dom
+
+    .catch((err) => {
+      console.log("Errore generico");
+    });
+};
+
+book();
